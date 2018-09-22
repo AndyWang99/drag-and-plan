8392e0b8d8112d2b71cbe38aa435d6fabe9770a1
Sufyan-Khan <khan.sufyan.cs@gmail.com>
2018-09-22 16:15:07 -0400

    Added Login page

---
+++ app/src/main/java/com/sodirea/drag_and_plan/LoginActivity.java
@@ -0,0 +1,141 @@
+package com.sodirea.drag_and_plan;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+
+    // UI references.
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d("tag", "createAccount:" + email);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("tag", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("tag", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signIn(String email, String password) {
+        Log.d("tag", "signIn:" + email);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("tag", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("tag", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+    }
+
+    private void updateUI(FirebaseUser user) {
+        //...
+    }
+
+    public void accClick(View v) {
+        int i = v.getId();
+        EditText emailText = (EditText) findViewById(R.id.emailText);
+        EditText passText = (EditText) findViewById(R.id.passText);
+
+        if (i == R.id.emailText) {
+            createAccount(emailText.getText().toString(), passText.getText().toString());
+        } else if (i == R.id.emailText) {
+            signIn(emailText.getText().toString(), passText.getText().toString());
+        } else {
+            signOut();
+        }
+    }
+
+}
+
--- app/src/main/java/com/sodirea/drag_and_plan/SetupActivity.java
+++ app/src/main/java/com/sodirea/drag_and_plan/SetupActivity.java
@@ -27,12 +27,20 @@ public class SetupActivity extends AppCompatActivity {
 
     public void onClick(View v) {
         EditText groupSizeText = (EditText) findViewById(R.id.groupSize);
+        EditText radiusText = (EditText) findViewById(R.id.radiusText);
         int amntOfPpl = Integer.parseInt(groupSizeText.getText().toString());
+        int radius = Integer.parseInt(radiusText.getText().toString());
         String category = categorySpinner.getSelectedItem().toString();
 
         Intent intent = new Intent(SetupActivity.this, MainActivity.class);
         intent.putExtra("amntOfPpl", amntOfPpl);
         intent.putExtra("category", category);
+        intent.putExtra("radius", radius);
         startActivity(intent);
+    }
+
+    public void toLogin(View v) {
+        Intent redir = new Intent(SetupActivity.this, LoginActivity.class);
+        startActivity(redir);
     }
 }
