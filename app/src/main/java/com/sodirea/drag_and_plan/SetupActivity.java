b59d6d88ee816e9579a3308815e90c50e13dd466
Sufyan-Khan <khan.sufyan.cs@gmail.com>
2018-09-22 04:43:40 -0400

    Added Setup Activity

--- app/src/main/java/com/sodirea/drag_and_plan/SetupActivity.java
+++ app/src/main/java/com/sodirea/drag_and_plan/SetupActivity.java
@@ -2,6 +2,9 @@ package com.sodirea.drag_and_plan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class SetupActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+    }
+
+    public void onClick(View v) {
+        Spinner categorySpinner = (Spinner) findViewById(R.id.categorySpinner);
+        EditText groupSizeText = (EditText) findViewById(R.id.groupSize);
+        int amntOfPpl = Integer.parseInt(groupSizeText.toString());
+        String category = categorySpinner.getSelectedItem().toString();
+
     }
 }
