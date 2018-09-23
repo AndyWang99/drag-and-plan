acafec34bc8522cf63316cedde95f327426e9438
Winnie Xu <winniexu@Winnies-MacBook-Pro.local>
2018-09-23 11:50:27 -0400

    profile additions

---
+++ app/src/main/java/com/sodirea/drag_and_plan/ProfileActivity.java
@@ -0,0 +1,31 @@
+package com.sodirea.drag_and_plan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        setContentView(R.layout.activity_profile);
+    }
+}
--- app/src/main/java/com/sodirea/drag_and_plan/SetupActivity.java
+++ app/src/main/java/com/sodirea/drag_and_plan/SetupActivity.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -34,22 +35,25 @@ public class SetupActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
     }
 
-<<<<<<< HEAD
-    public void onClick(View v) {
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
-=======
     public void toMain(View v) {
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
->>>>>>> 9757c4a2b697ba39d4aafd29261c32674318a75c
 
         EditText nameText = (EditText) findViewById(R.id.nameText);
         String[] name = nameText.getText().toString().split(" ");
+//        TextView profileName = (TextView) findViewById(R.id.NameField);
+        TextView education = (TextView) findViewById(R.id.EduField);
+        String edValue = education.getText().toString();
+        EditText interests = (EditText) findViewById(R.id.InterestField);
+        String[] interest = interests.getText().toString().split(" ");
 
         Map<String, Object> user = new HashMap<>();
         user.put("first", name[0]);
         user.put("last", name[1]);
+        user.put("education", edValue);
+        for (int i = 1; i < interest.length; ++i) {
+            user.put("interest" + i, interest[i]);
+        }
         user.put("key", currentUser.getUid());
 
         db.collection("users")
